f122daa2c4f327ec4a2f979777a338443749139f
hyunjulee <hyunju920424@gmail.com>
2017-01-11 15:24:00 +0900

    2nd commit

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
+
+func Sqrt(x float64) float64 {
+    z:=2.0
+    diff:=1.0
+    for diff>0.00000001 {
+        val1:=z
+        z=z-(z*z-x)/(2*z)
+        diff=val1-z
+
+    }
+    return z
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+    fmt.Println(Sqrt(2))
 }
